2bf2f15c2241474018bff9910757a125072b48d4
Jared Stanley <jstanley@purestorage.com>
2018-07-16 21:32:13 -0700

    working at a basic level, now going to layer on shapes

--- node_version/js/script.js
+++ node_version/js/script.js
@@ -3,11 +3,12 @@ _P.w = 400;
 _P.h = 300;
 _P.fs = require('fs');
 _P.Canvas = require('canvas');
+_P.Image = _P.Canvas.Image;
+_P.Shape = require('./shape');
+//
 _P.canvas = new _P.Canvas(_P.w, _P.h);
 _P.ctx = _P.canvas.getContext('2d');
-_P.Image = _P.Canvas.Image;
 _P.imgObj = new _P.Image();
-_P.Shape = require('./shape');
 _P.shape = new _P.Shape(_P);
 
 //
@@ -20,30 +21,26 @@ _P.App = {
 };
 _P.SrcImgMgr = {
 	imgList:[],
-	// path: "-enter-key-notebook-50710.jpeg",
-	path: "-businessman-person-140945.jpg",
+	// path: "-businessman-person-140945.jpg",
 	createImageList: function(){
-    console.log("createImageList....");
+    // console.log("createImageList....");
     _P.fs.readdir(_P.App.dir, function(err, items) {
       for (var i=0; i<items.length; i++) {
           // console.log(i);
           _P.SrcImgMgr.imgList.push(items[i])
       }
       console.log(_P.SrcImgMgr.imgList.length+" items counted");
-      console.log("....createImageList completed");
+      // console.log("....createImageList completed");
 			_P.SrcImgMgr.path = _P.SrcImgMgr.imgList[612]
-			_P.SrcImgMgr.loadNextImage(_P.App.dir+_P.SrcImgMgr.path);
-
+			var p = _P.SrcImgMgr.path;
+			_P.SrcImgMgr.loadNextImage(_P.App.dir+p);
     });
   },
 	loadNextImage:function(url){
 	    console.log("loadNextImage.....", url);
 	    _P.imgObj.onload = function(){
-	      console.log("imgObjloaded", _P.imgObj.width);
-	      // _App.ctx.globalCompositeOperation = "hard-light";
-	      // _App.ctx.drawImage(_App.imgObjObj, 0,0, _App.w, _App.h);
-	      //
-			_P.CnvMgr.consumeImage();
+	      // console.log("imgObjloaded", _P.imgObj.width);
+				_P.CnvMgr.consumeImage();
 	    }
 	    _P.imgObj.src = url;
 
@@ -57,26 +54,19 @@ _P.CnvMgr = {
 		var w = _P.canvas.width;
 		var h = _P.canvas.height;
 			//
-			_P.ctx.fillStyle="rgb(0,0,222)";
+			_P.ctx.fillStyle="rgb(22,222,0)";
 			_P.ctx.fillRect(0,0,w, h);
 			_P.ctx.globalCompositeOperation = "hard-light";
+			//draw loaded image onto canvas
 			_P.ctx.drawImage(_P.imgObj, 0,0, _P.imgObj.width, _P.imgObj.height, 0,0,w,h);
-			_P.ctx.fillStyle="rgb(211,50,22)";
-			_P.ctx.globalCompositeOperation = "screen";
-			_P.CnvMgr.drawShape();
-			// _P.shape.drawShape();
+			_P.ctx.fillStyle="rgba(211,50,22, 0.05)";
+			_P.ctx.globalCompositeOperation = "color-burn";
+			// _P.CnvMgr.drawShape();
+			_P.shape.drawShapeTest();
 			_P.CnvMgr.writeOutput();
 	},
 	writeOutput:function(){
 			_P.canvas.createPNGStream().pipe(_P.fs.createWriteStream('output/'+_P.SrcImgMgr.path));
-	},
-	drawShape:function(){
-		_P.ctx.beginPath();
-		for (var i = 1; i < 30; i++) {
-			var itm = {x:Math.random()*_P.w,y:Math.random()*_P.h};
-			_P.ctx.lineTo(itm.x, itm.y);
-		}
-		_P.ctx.fill();
 	}
 }
 //
--- node_version/js/shape.js
+++ node_version/js/shape.js
@@ -1,38 +1,19 @@
 var utils = require('./utils');
-//static class variables (internal)
-//global (to this module)
-const maxPoints= 10000;
 
-
-function createBaseShape(s) {
-  console.log("createBaseShape");
-  // console.log(this.shapes["shapeArr_0"]);
-  for (var i = 0; i < 4; i++) {
-    // if(this.totalPoints<this.maxPoints){
-      s.subdivisions++;
-      // console.log(i,subdivisions, s.shapes);
-      s.shapes["shapeArr_"+s.subdivisions] = s.subDivide(s.shapes["shapeArr_"+i], i);
-      // this.totalPoints+=shapes["shapeArr_"+this.subdivisions].length;
-    // }else{
-      // console.log(this.totalPoints,"hey we're jammed up w a ton of points so gonna chill rn");
-    // }
-  }
-  s.baseShape = s.shapes["shapeArr_"+s.subdivisions].slice();
-  s.shapes = {};
-  s.subdivisions = 0;
-}
-// new
 class Shape {
   constructor(app, color) {
+    this.maxPoints= 10000;
     this.subdivisions = 0;
     this.totalPoints = 0;
     this.app = app;
     this.color = color;
     this.shapes = { shapeArr_0: [] };
-    this.createStartingPoints();
-    // createBaseShape(this);
+    this.initStartingPoints();
+    // this.createBaseShape();
   }
-  createStartingPoints() {
+
+  initStartingPoints() {
+    // console.log("initStartingPoints() called");
     let num_pts = 9;
     let ang = 0;
     let rad = this.app.w*0.5;
@@ -42,32 +23,51 @@ class Shape {
       var _y = this.app.h/2+Math.sin(ang)*rad;
       ang+=step;
       this.shapes.shapeArr_0.push({x:_x, y:_y});
-
     }
   }
+
+  createBaseShape() {
+    console.log("createBaseShape() called....");
+    // console.log(this.shapes);
+    for (var i = 0; i < 4; i++) {
+        this.subdivisions++;
+        this.shapes["shapeArr_"+this.subdivisions] = this.subDivide(this.shapes["shapeArr_"+i]);
+    }
+
+    this.baseShape = this.shapes["shapeArr_"+this.subdivisions].slice();
+    // this.shapes = {};
+    // this.subdivisions = 0;
+    console.log("createBaseShape completed, baseShape has  ",this.baseShape.length, "  points now");
+  }
+
   subDivide(arr) {
-    // console.log("subdivide", arr);
+    // console.log("subdivide", arr.length);
+
     var tmparr=arr.slice();
+    // console.log("start",tmparr.length);
     var newArr = [];
     for (var i = 0; i < tmparr.length; i++) {
+
       if(i==0){
         //*|* first one
         newArr.push(tmparr[i]);
       }else{
         //*|* main loop
         var newPt = utils.createNewPoint(tmparr[i], tmparr[i-1]);
+        // console.log(newPt,"<<<<<");
         //push new point, then push older existing point
         newArr.push(newPt);
         newArr.push(arr[i]);
       }
+      // console.log("end",newArr.length);
     }
     //*|* last one! Wrap back to connect to the first point at the end of the loop
     var newPt = utils.createNewPoint(arr[arr.length-1], arr[0], 1);
     newArr.push(newPt);
-
+    // console.log(newArr.length);
     return newArr;
   }
-
+/*
   generateLayer(pos) {
     console.log("generateLayer");
     var tmparr = this.baseShape.slice();
@@ -77,15 +77,10 @@ class Shape {
     }
 
     this.shapes["shapeArr_"+pos]=tmparr.slice();
-  }
+  }*/
   drawShape(_arr) {
     // _arr = this.shapes[0];
-    return;
       this.app.ctx.fillStyle=this.color;
-      // this.app.ctx.fillStyle="rgba(155,10,10,0.013324)";
-
-      // this.app.ctx.fillStyle= "rgba(150,"+Math.round(Math.random()*255)+",255,0.025)";
-      // console.log(_arr, "$$$$$$$$$$$$$$$$$$$$$$$$$");
       this.app.ctx.beginPath();
       this.app.ctx.moveTo(_arr[0].x, _arr[0].y);
       for (var i = 1; i < _arr.length; i++) {
@@ -93,6 +88,39 @@ class Shape {
         this.app.ctx.lineTo(itm.x, itm.y);
       }
       this.app.ctx.fill();
+  }
+  drawShapeTest(){
+    console.log("drawShapeTest called");
+    // console.log(this.shapes);
+    var shapeData={};
+    this.createBaseShape();
+    // console.log(this.shapes);
+    var arr = this.baseShape;
+    //arr = this.subDivide(arr);
+    this.app.ctx.beginPath();
+    // console.log(this.shapes);
+    // var obj = this.shapes;
+    // Object.keys(obj).forEach(function(key){
+    //   console.log(key, obj[key]);
+    // })
+    // return;
+		for (var p in this.shapes) {
+      if (this.shapes.hasOwnProperty(p)) {
+        var curArr = this.shapes[p];
+        this.app.ctx.fillStyle="rgba("+Math.round(Math.random()*200)+","+Math.round(Math.random()*200)+",200,0.08)";
+
+        //console.log(this.shapes[p].length);
+        for (var i = 0; i < curArr.length; i++) {
+          var itm = {x:curArr[i].x, y:curArr[i].y};
+          this.app.ctx.lineTo(itm.x, itm.y);
+        }
+        this.app.ctx.fill();
+      }
+    }
+
+
+
+
   }
   test(){
     console.log('testing from shape is a success!!!!');
